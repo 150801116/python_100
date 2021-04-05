bda999167149f5628ec85b16eb9e3c9f40d99775
150801116 <1522672524@qq.com>
2021-04-05 14:48:30 +0800

    题目 时间函数举例4。

---
+++ python_100/实例094：time模块IV.py
@@ -0,0 +1,35 @@
+#题目 时间函数举例4。
+if __name__ == '__main__':
+    import time
+    import random
+
+    play_it = input('do you want to play it.(\'y\' or \'n\')')
+    while play_it == 'y':
+        c = input('input a character:\n')
+        i = random.randint(0,2**32) % 100
+        print ('please input number you guess:\n')
+        #很明显这个答案这边缺点number的输入。。。
+        start = time.perf_counter()
+        a = time.time()
+        guess = int(input('input your guess:\n'))
+        while guess != i:
+            if guess > i:
+                print('please input a little smaller')
+                guess = int(input('input your guess:\n'))
+            else:
+                print('please input a little bigger')
+                guess = int(input('input your guess:\n'))
+        end = time.perf_counter()
+        b = time.time()
+        var = (end - start) / 18.2
+        print (var)
+        # print 'It took you %6.3 seconds' % time.difftime(b,a))
+        if var < 15:
+            print ('you are very clever!')
+        elif var < 25:
+            print ('you are normal!')
+        else:
+            print ('you are stupid!')
+        print ('Congradulations')
+        print ('The number you guess is %d' % i)
+        play_it = input('do you want to play it.')
